e1537aec86a87a1ad8a4116482bf86ed86534061
PhucTHM <thmphuc0805@gmail.com>
2025-02-21 16:36:30 +0700

    add env

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -49,7 +49,7 @@ export default function DrawerAppBar(props) {
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [fetchData]);
 
   const handleDrawerToggle = () => {
     setMobileOpen((prevState) => !prevState);
