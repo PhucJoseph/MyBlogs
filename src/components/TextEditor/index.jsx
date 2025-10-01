efd5253bac662fe4cabbdeb6e27715d176713952
PhucTHM <thmphuc0805@gmail.com>
2025-10-01 14:26:02 +0700

    change LICENSE_KEY

--- src/components/TextEditor/index.jsx
+++ src/components/TextEditor/index.jsx
@@ -3,8 +3,7 @@ import { CKEditor, useCKEditorCloud } from "@ckeditor/ckeditor5-react";
 import PropTypes from "prop-types";
 import "./Editor.css";
 
-const LICENSE_KEY =
-  "eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3NzA4NTQzOTksImp0aSI6IjI2MDI4ODQzLTg5OTAtNDY0MC04ODhlLTEyZjM4MTliNTA2NiIsImxpY2Vuc2VkSG9zdHMiOlsiMTI3LjAuMC4xIiwibG9jYWxob3N0IiwiMTkyLjE2OC4qLioiLCIxMC4qLiouKiIsIjE3Mi4qLiouKiIsIioudGVzdCIsIioubG9jYWxob3N0IiwiKi5sb2NhbCJdLCJ1c2FnZUVuZHBvaW50IjoiaHR0cHM6Ly9wcm94eS1ldmVudC5ja2VkaXRvci5jb20iLCJkaXN0cmlidXRpb25DaGFubmVsIjpbImNsb3VkIiwiZHJ1cGFsIl0sImxpY2Vuc2VUeXBlIjoiZGV2ZWxvcG1lbnQiLCJmZWF0dXJlcyI6WyJEUlVQIl0sInZjIjoiNzQ4NDNjNDkifQ.9qcvVkXsd7wIKeJlHFHr41Vz5HzfQHiPFwDTKWAU9iLEKACUhTBmwyNIWjip8imLqs39QAfeGmGDOsElrNwO7Q";
+const LICENSE_KEY = "GPLs";
 
 export default function TextEditor({
   setContent,
